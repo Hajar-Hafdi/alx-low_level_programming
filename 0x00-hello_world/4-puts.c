cf2f851e8e7aba0e72491cc9ff929d5b2fdf44e6
Hajar <Hajarlakbir@outlook.com>
2024-06-20 10:56:30 -0700

    c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/*
+ * main: EntrY Point
+ *
+ * return: always 0 (success)
+ */
+int main(void)
+{
+	char sentence[] = "Programming is like building a multilingual puzzle";
+
+	puts(sentence);
+	return (0);
+}
