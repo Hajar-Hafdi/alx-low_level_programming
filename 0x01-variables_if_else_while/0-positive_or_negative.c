34049dc741db33370d57163b3911e59c085aae64
Hajar <Hajarlakbir@outlook.com>
2024-06-21 11:36:31 -0700

    if

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: using if loop to find if n is greater or less or equal to 0.
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n;
+
+	if (n < 0)
+	{
+		printf("is negative\n");
+	} else
+	{
+		printf("is positive\n");
+	} else
+	{
+		printf("is zero\n");
+	}
+	return (0);
+}
