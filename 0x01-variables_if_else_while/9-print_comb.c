8385e5f794a82797f6b1e31c6a7e03636d312b3d
Hajar <Hajarlakbir@outlook.com>
2024-06-21 14:26:28 -0700

    for

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: using for loop and putchar function to print numbers.
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int number;
+
+	for (number = 48; number <= 57; number++)
+	{
+		putchar(number);
+		putchar(' ');
+		putchar(',');
+	}
+	putchar('\n');
+	return (0);
+}
