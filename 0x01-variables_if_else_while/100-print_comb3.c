73cf2bc994d452a8b5190da8babf8f4ebc9c69e4
Hajar <Hajarlakbir@outlook.com>
2024-06-21 18:49:22 -0700

    for if

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: using for loop if loop and putchar function to print
+ *              all possible double digits without repetition
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int fd, sd;
+
+	for (fd = 48; fd <= 57; fd++)
+	{
+		for (sd = fd + 1; sd <= 57; sd++)
+		{
+			putchar(fd);
+			putchar(sd);
+			if (fd != 8 && sd != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
