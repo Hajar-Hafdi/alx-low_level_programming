04cbe168a596db857c97b251b8fef0c6a23f650e
Hajar <Hajarlakbir@outlook.com>
2024-06-25 15:31:04 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,17 @@
 #include "main.h"
-/***/
+
+/**
+ * print_alphabet_x10 - uses _putchar function to print
+ *                       alphabets in lowercase x10.
+*/
 void print_alphabet_x10(void);
 {
 	int letter, time;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for (time = 0; time <= 9; time++)
 	{
-		for (time = '0'; time <= 10; time++)
-		putchar(letter);
-		
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
+}
