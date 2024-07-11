439b07b21038208c51853dc845c14c4ad8f93568
Hajar <Hajarlakbir@outlook.com>
2024-07-10 17:40:40 -0700

    5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -14,7 +14,7 @@ int _sqrt_helper(int n, int i)
 		return (-1);
 	if (i * i == n)
 		return (i);
-	return (sqrt_helper(n, i + i));
+	return (_sqrt_helper(n, i + i));
 }
 /**
  * _sqrt_recursion - returns the natural square root of a number
@@ -28,5 +28,5 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (sqrt_recursion(n, 0));
+	return (_sqrt_recursion(n, 0));
 }
