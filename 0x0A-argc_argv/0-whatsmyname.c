0268530c184f3704e93de5af36c6eb54c83c5cd3
Hajar <Hajarlakbir@outlook.com>
2024-07-12 21:28:55 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints program's name
+ *
+ * @argc: int
+ * @argv: list
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
