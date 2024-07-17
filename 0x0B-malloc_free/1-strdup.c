aec6fb5caf961c163de2634b56b9dc942d91e174
Hajar <Hajarlakbir@outlook.com>
2024-07-16 19:59:35 -0700

    1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib>
+
+/**
+ * *_strdup - return a pointer to a newly allocated space in memo
+ *
+ * @str: string
+ *
+ * Return: 0 (Success)
+ */
+char *_strdup(char *str)
+{
+	int j = 0, sz = 0;
+	char *n;
+
+	if (str == NULL)
+		return (NULL);
+	for (; str[sz] != '\0'; sz++)
+
+	n = malloc(sz * sizeof(*str) + 1);
+
+	if (n == 0)
+		return (NULL);
+	else if
+	{
+		for (; j < sz; j++)
+			n[j] = str[j];
+	}
+	return (n);
+}
