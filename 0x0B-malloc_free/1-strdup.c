9e85e2afe73939d0bdaa70cece9de9641abd28a0
Hajar <Hajarlakbir@outlook.com>
2024-07-16 20:10:11 -0700

    2

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,13 +16,14 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 	for (; str[sz] != '\0'; sz++)
+		;
 
 	n = malloc(sz * sizeof(*str) + 1);
 
 	if (n == 0)
 		return (NULL);
 	else if
-		(for (; j < sz; j++)
+		for (; j < sz; j++)
 			n[j] = str[j];
 	return (n);
 }
