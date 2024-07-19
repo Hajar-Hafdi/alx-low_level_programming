4df648c4f1eaf90f78341320cd4fe55fd79a92b5
Hajar <Hajarlakbir@outlook.com>
2024-07-18 18:26:16 -0700

    fixes

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,5 +8,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 char *_memst(char *s, char b, unsigned int n);
+void simple_print_buffer(char *buffer, unsigned int size);
 
 #endif
