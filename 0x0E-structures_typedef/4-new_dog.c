b58d64af93610c26970214e5c733188fa8a291aa
Hajar <Hajarlakbir@outlook.com>
2024-07-22 20:26:47 -0700

    4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -60,7 +60,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	dog->owner = malloc(sizeof(char) * (_strlenn(owner) = 1));
+	dog->owner = malloc(sizeof(char) * (_strlenn(owner) + 1));
 	if ((*dog).owner == NULL)
 	{
 		free(dog->name);
