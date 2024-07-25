0f99d550cc9fab387a94472d1cac10068e897bec
Hajar <Hajarlakbir@outlook.com>
2024-07-25 13:17:16 -0700

    task03

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two integers
+ *
+ * @a: 1st int
+ * @b: 2nd int
+ *
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - substractes 2 ints
+ *
+ * @a: 1st int
+ * @b: 2nd int
+ *
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies 2 ints
+ *
+ * @a: 1st int
+ * @b: 2nd int
+ *
+ * Return: result of multiplication
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - function that executes a division
+ *
+ * @a: 1st int
+ * @b: 2nd int
+ *
+ * Return: result of the division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - modulo 2 integers
+ *
+ * @a: 1st int
+ * @b: 2nd int
+ *
+ * Return: remainder of int
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,9 @@
 #define _FUNCTION_POINTERS_H_
 
 #include <stdlib.h>
+#include <stdio.h>
+
+
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
