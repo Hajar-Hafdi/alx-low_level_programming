7672af85857d58c45042218790e67e7496a8bc63
HajarHAFDI <hajarlakbir@outlook.com>
2024-08-01 14:52:43 -0700

    added 2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * *add_node - adds a new node at the beginning of a list_t list
+ *
+ * @head: adress of ptr to head node
+ * @str: string field of node
+ *
+ * Return: size of list
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newer_head = malloc(sizeof(list_t));
+
+	if (!head || !newer_head)
+		return (NULL);
+	if (str)
+	{
+		newer_hear->str = strdup(str);
+		if (!newer_head->str)
+		{
+			free(newer_head);
+			return (NULL);
+		}
+		newer_head->len = _stringlen(newer_head->str);
+	}
+	newer_head->next = *head;
+	*head = newer_head;
+	return (newer_head);
+}
