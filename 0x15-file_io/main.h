53201672faad2cc49966a114ea6f4b6cbf1137e7
HajarHAFDI <hajarlakbir@outlook.com>
2024-08-12 21:50:06 -0700

    fixed more errors

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,6 +13,16 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int main(int arc, char **arv);
+void output_magic(Elf64_Ehdr h);
+void output_class(Elf64_Ehdr h);
+void output_data(Elf64_Ehdr h);
+void output_version(Elf64_Ehdr h);
+void output_osabi(Elf64_Ehdr h);
+void output_more_osabi(Elf64_Ehdr h);
+void output_abiversion(Elf64_Ehdr h);
+void output_type(Elf64_Ehdr h);
+void output_entry(Elf64_Ehdr h);
+int main(int arc, char **arv);
 
 
 #endif 
