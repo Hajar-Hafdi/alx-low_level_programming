dfbbf791828fa06e19c76e5a56c34eb6935184cb
Hajar Hafdi <hajarlakbir@outlook.com>
2024-09-19 13:43:09 -0700

    fixed errors andded 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a dlistint_t list
+ *
+ * @head: add of ptr
+ *
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *nd;
+
+	while (head)
+	{
+		nd = head;
+		head = head->next;
+		free(nd)
+	}
+}
