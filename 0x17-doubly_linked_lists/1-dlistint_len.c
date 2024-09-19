ebb9a2c09cc37ca3113e72ec07ba45c88a81f2e9
Hajar Hafdi <hajarlakbir@outlook.com>
2024-09-19 12:36:24 -0700

    added file 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - output len of dl
+ *
+ * @h: add of hea nd
+ *
+ * Return: size of list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	siz_t o = 0;
+
+	while (h)
+	{
+		h = h->next;
+		o++;
+	}
+	return (o);
+}
+
