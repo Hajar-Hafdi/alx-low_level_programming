b4d261dc309e155332ded1c8e11612874236d285
Hajar-Hafdi <Hajarlakbir@outlook.com>
2024-10-11 19:06:58 -0700

    fixed errors

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -12,22 +12,21 @@ void hash_table_delete(hash_table_t *ht)
 	hash_table_t *hed = ht;
 	hash_node_t *nod, *tp;
 	unsigned long int u;
+	if (ht == NULL)
+		return;
 
 	for (u = 0; u < ht->size; u++)
 	{
-		if (ht->array[u] != NULL)
+		nod = ht->array[u];
+		while (nod != NULL)
 		{
-			node = ht->array[u];
-			while (nod != NULL)
-			{
-				tp = nod->next;
-				free(nod->key);
-				free(nod->value);
-				free(nod);
-				nod = tp;
-			}
+			tp = nod->next;
+			free(nod->key);
+			free(nod->value);
+			free(nod);
+			nod = tp;
 		}
 	}
-	free(hed->array);
-	free(hed);
+	free(ht->array);
+	free(ht);
 }
